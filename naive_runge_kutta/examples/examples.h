b36e5d7480bb815c459244587c128ecb7eaf0857
CODER591 <mazorchyk@gmail.com>
2022-04-16 10:31:09 +0300

    Separating examples from main

---
+++ naive_runge_kutta/examples/examples.h
@@ -0,0 +1,7 @@
+#ifndef EXAMPLES_H
+#define EXAMPLES_H
+
+void mock_example3();
+
+
+#endif /* EXAMPLES_H */
--- naive_runge_kutta/main.cpp
+++ naive_runge_kutta/main.cpp
@@ -1,25 +1,9 @@
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
 
-#include "common/functions.h"
-#include "second/runge_second_order.h"
-#include "forth/equation/runge_forth_order.h"
+#include "examples/examples.h"
 
 int main () {
-    Example3 functions;
-    
-    double N = 10000;                 //Amount of the intervals of [a,b]
-    double x_begin = 3.14 / 3.0;      // a
-    double x_end = x_begin + 0.5;     // b
-    //y(x_0) = y_0;
-    double y0 = 0;
-    std::cout << "Runge-kutta 2nd order: " << runge_kutta_2nd_order_1f_(x_begin, x_end, y0, N, &functions) <<std::endl;
-    std::cout << "Strict solution: " << functions.f_sol(x_end) <<std::endl;
-    
-    std::cout << "-----------------" << std::endl;
-    std::cout << "Runge-kutta 4rh order: " << runge_kutta_4rh_order_1f_(x_begin, x_end, y0, N, &functions) <<std::endl;
-    std::cout << "Strict solution: " << functions.f_sol(x_end) <<std::endl;
+
+    mock_example3();
 
     return 0;
 }
