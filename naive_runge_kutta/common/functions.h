0f0272305cbc249cb547675b9622d74be966ddd3
CODER591 <mazorchyk@gmail.com>
2022-04-16 10:46:40 +0300

    Refactoring

--- naive_runge_kutta/common/functions.h
+++ naive_runge_kutta/common/functions.h
@@ -6,19 +6,53 @@
 
 
 struct AbstractExample {
+    //y(x_0) = y_0;
+    virtual double get_x_begin() = 0;
+    virtual double get_y0() = 0;
+
     virtual double f_sol(double x) = 0;
     virtual double f_shtrih(const double x, const double y) = 0;
     virtual ~AbstractExample() {}
+
+    double get_step() {
+        return 0.5;
+    }
+
+    double get_x_end() {
+        return get_x_begin() + get_step();
+    }
 };
 
 /* page 330, example3 */
 struct Example3 : public AbstractExample {
+    double get_x_begin() override {
+        // pi/3
+        return 3.14/3.0;
+    }
+    
+    double get_y0() override {
+        // y(pi/3) = 0;
+        return 0;
+    }
+
     double f_sol(double x) override {
         return 2.0 - 4.0 * std::cos(x);
     }
 
     double f_shtrih(const double x, const double y) override {
         return (2.0 - y) * std::tan(x);
+    }
+};
+/* !example 3 */
+
+/* page 330, example5 */
+struct Example5 : public AbstractExample {
+    double f_sol(double x) override {
+        return 2 * std::exp(x) - 2*x + 1;
+    }
+
+    double f_shtrih(const double x, const double y) override {
+        return y + 2*x - 3;
     }
 };
 /* !example 3 */
--- naive_runge_kutta/examples/examples.cc
+++ naive_runge_kutta/examples/examples.cc
@@ -12,10 +12,10 @@ void mock_example3() {
     Example3 functions;
     
     double N = 10000;                 //Amount of the intervals of [a,b]
-    double x_begin = 3.14 / 3.0;      // a
-    double x_end = x_begin + 0.5;     // b
+    double x_begin = functions.get_x_begin();    // a
+    double x_end = functions.get_x_end();     // b
     //y(x_0) = y_0;
-    double y0 = 0;
+    double y0 = functions.get_y0();
     std::cout << "Runge-kutta 2nd order: " << runge_kutta_2nd_order_1f_(x_begin, x_end, y0, N, &functions) <<std::endl;
     std::cout << "Strict solution: " << functions.f_sol(x_end) <<std::endl;
     
