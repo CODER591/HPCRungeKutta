ff82cf09f749f3d9327696b8135cbe9c2a887a55
CODER591 <mazorchyk@gmail.com>
2022-04-18 00:11:39 +0300

    Adding initial threading attempt

--- naive_runge_kutta/forth/equation/runge_forth_order.cc
+++ naive_runge_kutta/forth/equation/runge_forth_order.cc
@@ -3,7 +3,7 @@
 #include "common/examples.h"
 #include "forth/equation/runge_forth_order.h"
 
-#include <functional>
+#include <thread>
 
 double runge_kutta_4rh_order_1f_(const double x_begin,
                            const double x_end,
@@ -17,6 +17,33 @@ double runge_kutta_4rh_order_1f_(const double x_begin,
 
   for (double x_n = x_begin; x_n < x_end; x_n += h) {
       double k1 = h * function->f_shtrih(x_n, y_n);
+      double k2 = h * function->f_shtrih(x_n + h/2.0, y_n + k1/2.0);
+      double k3 = h * function->f_shtrih(x_n + h/2.0, y_n + k2/2.0);
+      double k4 = h * function->f_shtrih(x_n + h, y_n + k3);
+      y_n = y_n + one_sixth*(k1 + 2.0*k2 + 2.0*k3 + k4);
+  }
+  return y_n;
+}
+
+double runge_kutta_4rh_order_1f_threading(const double x_begin,
+                           const double x_end,
+                           const double y_0,
+                           const int N, 
+                           AbstractExample* function) {
+  
+  double h = (x_end - x_begin) / static_cast <double> (N);
+  double y_n = y_0;
+  double one_sixth = 1.0/6.0;
+
+  for (double x_n = x_begin; x_n < x_end; x_n += h) {
+      
+      
+      double k1 = 0.0; //variable on stack, WARNING HERE!
+      std::thread k1_calc_thread([=, &k1]() mutable {
+        k1 = h * function->f_shtrih(x_n, y_n);
+      });
+      k1_calc_thread.join();
+
       double k2 = h * function->f_shtrih(x_n + h/2.0, y_n + k1/2.0);
       double k3 = h * function->f_shtrih(x_n + h/2.0, y_n + k2/2.0);
       double k4 = h * function->f_shtrih(x_n + h, y_n + k3);
--- naive_runge_kutta/main.cpp
+++ naive_runge_kutta/main.cpp
@@ -1,11 +1,28 @@
 
 #include "tests/tests.h"
 
+#include <chrono>
+#include <iostream>
+#include <functional>
+using namespace std::chrono;
+
+
+void run_and_measure_time(std::function<void()> func) {
+    auto start = high_resolution_clock::now();
+
+    func();
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    std::cout << "Time taken by function: "
+         << duration.count() << " microseconds" << std::endl;
+}
+
 int main () {
 
-    mock_example3();
+    run_and_measure_time(&mock_example3);
 
-    mock_system_example_brusselator();
+    //mock_system_example_brusselator();
 
     return 0;
 }
